84d283a4bfd6819a2a64e0b2b77b0df15c9aba9b
Costin98 <morun.costin@yahoo.com>
2023-01-30 17:08:06 +0200

    App 4 start
    Load the excel files into data frames
    
    I have created a minimal PDF for each excel file
    
    added a date
    
    add a table
    
    Total price header company and logo
    invoices+main.py
    
    +one experiment regarding PDF
    text_files+Title.py

--- Titles.py
+++ Titles.py
@@ -12,8 +12,18 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     name = filename.title()
 
+
     pdf.set_font(family='times', size=20, style='B')
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+    with open(filepath,'r') as file:
+        content=file.read()
+        pdf.set_font(family="Times", size=12)
+        pdf.multi_cell(w=0, h=6, txt=content)
 
-    pdf.cell(w=50, h=10, txt=name, ln=1)
 
-    pdf.output('output.pdf')
+
+        pdf.cell(w=50, h=10, txt=name, ln=1)
+
+
+
+pdf.output('output.pdf')
