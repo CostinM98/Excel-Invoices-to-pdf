c739c9efb0d4ba7377848c8766930b708e5863c1
Costin98 <morun.costin@yahoo.com>
2023-01-30 15:38:28 +0200

    App 4 start
    Load the excel files into data frames
    
    I have created a minimal PDF for each excel file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from fpdf import FPDF
 
+from pathlib import Path
 import glob
 
 filepaths = glob.glob('invoices/*.xlsx')
@@ -7,5 +9,13 @@ filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name='Sheet 1')
+    pdf=FPDF(orientation='p',unit='mm',format='A4')
+    pdf.add_page()
 
-    print(df)
+    filename=Path(filepath).stem
+    invoiceNR=filename.split("-")[0]
+    pdf.set_font(family='times',size=20,style='B')
+
+    pdf.cell(w=50,h=10,txt=f'Invoice no.{invoiceNR}')
+
+    pdf.output(f'PDFs/{filename}.pdf')
